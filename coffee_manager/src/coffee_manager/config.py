13c21ac012066903c90aecc697d48ebf1334e7fa
pgierzat <01187225@pw.edu.pl>
2026-03-28 11:46:43 +0100

    config for db

---
+++ coffee_manager/src/coffee_manager/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+settings = Settings()
