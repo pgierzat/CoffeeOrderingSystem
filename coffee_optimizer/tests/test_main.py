96518a837cedbdcdfe4674194791230490460ee5
pgierzat <01187225@pw.edu.pl>
2026-05-12 23:48:31 +0200

    ruff format fix

--- coffee_optimizer/tests/test_main.py
+++ coffee_optimizer/tests/test_main.py
@@ -51,7 +51,9 @@ def full_mock_data():
             else (
                 8.0
                 if (d == "D1" and lvl == 2)
-                else 9.5 if (d == "D2" and lvl == 1) else 7.5
+                else 9.5
+                if (d == "D2" and lvl == 1)
+                else 7.5
             )
         )
         for d in distributors
